2c5eb29cb58ca6bc8a59adf442f057cb2f3b60c0
Vsanotskyi <vsanotskyi@gmail.com>
2024-07-18 14:22:47 +0300

    add footer for tablet/laptop

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -53,159 +53,164 @@ const Footer: React.FC = () => {
 
   return (
     <footer className={s.footer}>
-      <div className={s.footer_content}>
-        <div className={s.footer_main}>
-          <div className={s.container}>
-            <h3 className={s.container_title}>Компанія</h3>
-            <ul>
-              <li>
-                <Link
-                  className={s.container_link}
-                  to={'/'}
-                  onClick={handleClick}
-                >
-                  Головна
-                </Link>
-              </li>
-              <li>
-                <Link
-                  className={s.container_link}
-                  to={'/about'}
-                  onClick={handleClick}
-                >
-                  Про нас
-                </Link>
-              </li>
-              <li>
-                <Link
-                  className={s.container_link}
-                  to={'/specialists'}
-                  onClick={handleClick}
-                >
-                  Майстри
-                </Link>
-              </li>
-              <li>
-                <Link
-                  className={s.container_link}
-                  to={'/services'}
-                  onClick={handleClick}
-                >
-                  Послуги
-                </Link>
-              </li>
-              <li>
-                <Link
-                  className={s.container_link}
-                  to={'/contacts'}
-                  onClick={handleClick}
-                >
-                  Контакти
-                </Link>
-              </li>
-            </ul>
+      <div className={s.footer_main}>
+        <div className={s.footer_content}>
+          <div className={s.footer_blocks}>
+            <div className={s.footer_block}>
+              <h3 className={s.footer_block_title}>Компанія</h3>
+              <ul>
+                <li>
+                  <Link
+                    className={s.footer_block_link}
+                    to={'/'}
+                    onClick={handleClick}
+                  >
+                    Головна
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className={s.footer_block_link}
+                    to={'/about'}
+                    onClick={handleClick}
+                  >
+                    Про нас
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className={s.footer_block_link}
+                    to={'/specialists'}
+                    onClick={handleClick}
+                  >
+                    Майстри
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className={s.footer_block_link}
+                    to={'/services'}
+                    onClick={handleClick}
+                  >
+                    Послуги
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className={s.footer_block_link}
+                    to={'/contacts'}
+                    onClick={handleClick}
+                  >
+                    Контакти
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className={s.footer_block}>
+              <h3 className={s.footer_block_title}>Послуги</h3>
+              <ul>
+                <li>
+                  <Link
+                    className={s.footer_block_link}
+                    to={'/process'}
+                    onClick={handleClick}
+                  >
+                    Волосся
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className={s.footer_block_link}
+                    to={'/process'}
+                    onClick={handleClick}
+                  >
+                    Нігті
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className={s.footer_block_link}
+                    to={'/process'}
+                    onClick={handleClick}
+                  >
+                    Обличчя
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className={s.footer_block_link}
+                    to={'/process'}
+                    onClick={handleClick}
+                  >
+                    Барбер послуги
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className={s.footer_block_link}
+                    to={'/process'}
+                    onClick={handleClick}
+                  >
+                    Послуги від учнів*
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className={s.footer_block}>
+              <h3 className={s.footer_block_title}>Підтримка</h3>
+              <ul>
+                <li>
+                  <a className={s.footer_block_link} href="tel:+380999999999">
+                    +380 99 999 99 99
+                  </a>
+                </li>
+                <li>
+                  <a className={s.footer_block_link}
+                     href="mailto:info@tutgarno.ua">
+                    info@tutgarno.ua
+                  </a>
+                </li>
+                <li>
+                  <Link
+                    className={s.footer_block_link}
+                    to={'/process'}
+                    onClick={handleClick}
+                  >
+                    Часті запитання
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </div>
-          <div className={s.container}>
-            <h3 className={s.container_title}>Послуги</h3>
-            <ul>
-              <li>
-                <Link
-                  className={s.container_link}
-                  to={'/process'}
-                  onClick={handleClick}
-                >
-                  Волосся
-                </Link>
-              </li>
-              <li>
-                <Link
-                  className={s.container_link}
-                  to={'/process'}
-                  onClick={handleClick}
-                >
-                  Нігті
-                </Link>
-              </li>
-              <li>
-                <Link
-                  className={s.container_link}
-                  to={'/process'}
-                  onClick={handleClick}
-                >
-                  Обличчя
-                </Link>
-              </li>
-              <li>
-                <Link
-                  className={s.container_link}
-                  to={'/process'}
-                  onClick={handleClick}
-                >
-                  Барбер послуги
-                </Link>
-              </li>
-              <li>
-                <Link
-                  className={s.container_link}
-                  to={'/process'}
-                  onClick={handleClick}
-                >
-                  Послуги від учнів*
-                </Link>
-              </li>
-            </ul>
-          </div>
-          <div className={s.container}>
-            <h3 className={s.container_title}>Підтримка</h3>
-            <ul>
-              <li>
-                <a className={s.container_link} href="tel:+380999999999">
-                  +380 99 999 99 99
-                </a>
-              </li>
-              <li>
-                <a className={s.container_link} href="mailto:info@tutgarno.ua">
-                  info@tutgarno.ua
-                </a>
-              </li>
-              <li>
-                <Link
-                  className={s.container_link}
-                  to={'/process'}
-                  onClick={handleClick}
-                >
-                  Часті запитання
-                </Link>
-              </li>
-            </ul>
-          </div>
+          <FooterForm />
         </div>
-        <FooterForm />
-        <Link className={s.footer_logo} to={'/'} onClick={handleClick}>
-          <Icon id={'icon-Logo'} style={s.footer_logo_icon} />
-        </Link>
-        <ul className={s.footer_social_list}>
-          <li>
-            <a href="#" className={s.footer_social_list_link}>
-              <Icon id={'icon-pinterest'} style={s.footer_social_list_icon} />
-            </a>
-          </li>
-          <li>
-            <a href="#" className={s.footer_social_list_link}>
-              <Icon id={'icon-TikTok'} style={s.footer_social_list_icon} />
-            </a>
-          </li>
-          <li>
-            <a href="#" className={s.footer_social_list_link}>
-              <Icon id={'icon-Facebook'} style={s.footer_social_list_icon} />
-            </a>
-          </li>
-          <li>
-            <a href="#" className={s.footer_social_list_link}>
-              <Icon id={'icon-insta'} style={s.footer_social_list_icon} />
-            </a>
-          </li>
-        </ul>
+        <div className={s.footer_second}>
+          <Link className={s.footer_logo} to={'/'} onClick={handleClick}>
+            <Icon id={'icon-Logo'} style={s.footer_logo_icon} />
+          </Link>
+          <ul className={s.footer_social_list}>
+            <li>
+              <a href="#" className={s.footer_social_list_link}>
+                <Icon id={'icon-pinterest'} style={s.footer_social_list_icon} />
+              </a>
+            </li>
+            <li>
+              <a href="#" className={s.footer_social_list_link}>
+                <Icon id={'icon-TikTok'} style={s.footer_social_list_icon} />
+              </a>
+            </li>
+            <li>
+              <a href="#" className={s.footer_social_list_link}>
+                <Icon id={'icon-Facebook'} style={s.footer_social_list_icon} />
+              </a>
+            </li>
+            <li>
+              <a href="#" className={s.footer_social_list_link}>
+                <Icon id={'icon-insta'} style={s.footer_social_list_icon} />
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
       <div>
         <ul className={s.footer_details}>
